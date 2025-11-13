ead6209af8b153fac1b8896a4f2149d00309f9ba
lizeth <lizeth11103@gmail.com>
2025-11-13 18:56:40 -0500

    Finalizacion de Cuenta

--- Cuenta.cs
+++ Cuenta.cs
@@ -6,8 +6,42 @@ using System.Threading.Tasks;
 
 namespace CajeroLitePOO
 {
-    internal class Cuenta
+    public class Cuenta
     {
-      
+        public double Saldo { get; private set; }
+
+        public Cuenta(double saldoInicial)
+        {
+            Saldo = saldoInicial >= 0 ? saldoInicial : 0;
+        }
+
+        public void Depositar(double monto)
+        {
+            if (monto <= 0)
+                Console.WriteLine("❌ El monto debe ser positivo.");
+            else
+            {
+                Saldo += monto;
+                Console.WriteLine($"✅ Depósito exitoso. Nuevo saldo: ${Saldo}");
+            }
+        }
+
+        public void Retirar(double monto)
+        {
+            if (monto <= 0)
+                Console.WriteLine("❌ El monto debe ser positivo.");
+            else if (monto > Saldo)
+                Console.WriteLine("❌ Fondos insuficientes.");
+            else
+            {
+                Saldo -= monto;
+                Console.WriteLine($" Retiro exitoso. Nuevo saldo: ${Saldo}");
+            }
+        }
+
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine($" Saldo disponible: ${Saldo}");
+        }
     }
 }

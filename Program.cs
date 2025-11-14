677142f32877db62d0593a6dc9e96022731cbe6f
SharonC23 <carrillosharon263@gmail.com>
2025-11-13 19:02:57 -0500

    PROGRAM TERMINADO

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CajeroLitePOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario(1, "Daniel", 1234, 500000);
+            Cajero cajero = new Cajero(usuario);
+            cajero.Iniciar();
+        }
+    }
+}
